a628f56a6f6a64412492b9ccf0ca3a9ec240f631
Crowdbotics <team@crowdbotics.com>
2024-06-20 09:48:37 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "scrapepostbot-48502",
+  url: "https://scrapepostbot-48502.botics.co",
+  api: "https://scrapepostbot-48502.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
